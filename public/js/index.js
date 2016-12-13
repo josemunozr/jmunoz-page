e0281bce4a939a2a5ae40d684abd1c37267fb29c
José Muñoz Ruiz <jarmunozr@gmail.com>
2016-12-12 23:57:16 -0300

    active itemMenu

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,32 @@
     }
   }
 
-  $(window).scroll(sticky_navigate);
+  function active_itemMenu() {
+    var home_top = $("#home").offset().top;
+    var portafolio_top = $("#portafolio").offset().top;
+    /*var aboutme_top = $("#about-me").offset().top;
+    var contact_top = $("#contact").offset().top;*/
+
+    if($(window).scrollTop() < portafolio_top){
+      $(".nav-item.home").addClass('active');
+    }else {
+      $(".nav-item.home").removeClass('active');
+    }
+
+    if($(window).scrollTop() >= portafolio_top){
+      $(".nav-item.pf").addClass('active');
+    }else {
+      $(".nav-item.pf").removeClass('active');
+    }
+
+  }
+
+  $(window).scroll(function () {
+    sticky_navigate();
+    active_itemMenu();
+  });
+
+  active_itemMenu();
   sticky_navigate();
 
 }());
